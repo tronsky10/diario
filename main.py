88bc8a70bd99e026da3e1630841b6c8522dc7cda
Tronsky <sinkop@gmail.com>
2025-05-13 17:10:59 +0200

    interfaz tkinter básica
    interfaz tkinter básica

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from datetime import date, datetime
+from tkinter import *
+from tkinter import ttk
+
+# tkinter básico
+root = Tk()
+root.title("Diario personal")
+ttk.Button(root, text="Añadir nota").grid(column=1, row=2)
+root.mainloop()
+
 
 # Llamada a la fecha y el ahora
 today = date.today()
